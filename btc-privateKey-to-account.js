6729be55a902c6b117adb851a472161d5eb2ec5b
sarathkaleswaram <ksb994@gmail.com>
2020-04-08 15:41:14 +0530

    updates

--- btc-privateKey-to-account.js
+++ btc-privateKey-to-account.js
@@ -1,6 +1,8 @@
-var bitcoin = require("bitcoinjs-lib");
+// cQNmJ6yuu3Z3UBjsxwbpUDX7FKEYi2E6X7oiGzvraEd9ErX6BCw4 
 
-const keyPair = bitcoin.ECPair.makeRandom();
+const bitcoin = require('bitcoinjs-lib');
+
+const keyPair = bitcoin.ECPair.fromWIF("L1Kzcyy88LyckShYdvoLFg1FYpB5ce1JmTYtieHrhkN65GhVoq73");
 const privateKey = keyPair.toWIF();
 const publicKey = keyPair.publicKey.toString("hex");
 const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
