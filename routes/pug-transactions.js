39b153e0f42f005c00386cef6c0294a30a89d813
sarathkaleswaram <ksb994@gmail.com>
2020-10-16 13:42:34 +0530

    total amounts sum

--- routes/pug-transactions.js
+++ routes/pug-transactions.js
@@ -9,7 +9,15 @@ var pugTransactions = function (req, res) {
 
     transactions.find({}).sort({ timeStamp: -1 }).then((transactions) => {
         logger.debug('Transactions length:', transactions.length)
-        res.render('transactions', { transactions: transactions, moment: require('moment') })
+        var totals = {
+            btc: transactions.filter(tx => tx.type === 'btc').reduce((sum, tx) => sum + (parseFloat(tx.amount) || 0), 0),
+            eth: transactions.filter(tx => tx.type === 'eth').reduce((sum, tx) => sum + (parseFloat(tx.amount) || 0), 0),
+            grt: transactions.filter(tx => tx.type === 'grt').reduce((sum, tx) => sum + (parseFloat(tx.amount) || 0), 0),
+            jan: transactions.filter(tx => tx.type === 'jan').reduce((sum, tx) => sum + (parseFloat(tx.amount) || 0), 0),
+            satx: transactions.filter(tx => tx.type === 'satx').reduce((sum, tx) => sum + (parseFloat(tx.amount) || 0), 0),
+        }
+        logger.debug('Totals', totals)
+        res.render('transactions', { transactions: transactions, totals: totals, moment: require('moment') })
     }, error => {
         logger.error(error)
         res.render('transactions', { error: true, message: error.toString() })
